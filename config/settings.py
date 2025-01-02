6899ca07baddee594561814ca75c9c318170d4b9
Saburi <catdogbird1192@gmail.com>
2025-01-02 23:44:08 +0900

    ALLOWED_HOSTS追加

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'django-insecure-6p9qnj--(o0r9gs)z(cda7fdzxb*)4ef7$@std%6964o*c*_c3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.168.88.217']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '54.168.88.217']
 
 
 # Application definition
